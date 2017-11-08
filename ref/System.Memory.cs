c56cfab48fdcf2d43b5ef35c69a996f9383e0061
Stephen Toub <stoub@microsoft.com>
2017-11-08 12:55:00 -0500

    Add MemoryMarshal.AsMemory

--- ref/System.Memory.cs
+++ ref/System.Memory.cs
@@ -127,6 +127,7 @@ namespace System
         public static ReadOnlySpan<byte> AsBytes<T>(this ReadOnlySpan<T> source) where T : struct { throw null; }
         
         public static ReadOnlySpan<TTo> NonPortableCast<TFrom, TTo>(this ReadOnlySpan<TFrom> source) where TFrom : struct where TTo : struct { throw null; }
+
         public static bool TryGetString(this ReadOnlyMemory<char> readOnlyMemory, out string text, out int start, out int length) { throw null; }
     }
 
@@ -368,3 +369,10 @@ namespace System.Buffers.Text
     }
 }
 
+namespace System.Runtime.InteropServices
+{
+    public static class MemoryMarshal
+    {
+        public static Memory<T> AsMemory<T>(ReadOnlyMemory<T> readOnlyMemory) { throw null; }
+    }
+}
