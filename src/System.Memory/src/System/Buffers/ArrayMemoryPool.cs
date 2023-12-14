5b5cfa5ef673845939448fc3b148d2c488da9c82
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-12-14 13:58:27 -0800

    Migrate System.Memory from corefx 2.1

---
+++ src/System.Memory/src/System/Buffers/ArrayMemoryPool.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+namespace System.Buffers
+{
+    internal sealed partial class ArrayMemoryPool<T> : MemoryPool<T>
+    {
+        private const int s_maxBufferSize = int.MaxValue;
+        public sealed override int MaxBufferSize => s_maxBufferSize;
+
+        public sealed override IMemoryOwner<T> Rent(int minimumBufferSize = -1)
+        {
+            if (minimumBufferSize == -1)
+                minimumBufferSize = 1 + (4095 / Unsafe.SizeOf<T>());
+            else if (((uint)minimumBufferSize) > s_maxBufferSize)
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.minimumBufferSize);
+
+            return new ArrayMemoryPoolBuffer(minimumBufferSize);
+        }
+
+        protected sealed override void Dispose(bool disposing) { }  // ArrayMemoryPool is a shared pool so Dispose() would be a nop even if there were native resources to dispose.
+    }
+}
