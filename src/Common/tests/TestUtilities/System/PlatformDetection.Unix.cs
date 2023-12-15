b70ebbc7e3b9add9514f46bb81a2b2292c4db75c
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-12-14 16:58:12 -0800

    Bring in from runtime the latest version of PlatformDetection. Remove unused APIs, add missing partial APIs.

--- src/Common/tests/TestUtilities/System/PlatformDetection.Unix.cs
+++ src/Common/tests/TestUtilities/System/PlatformDetection.Unix.cs
@@ -1,101 +1,172 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.IO;
-using System.Xml.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using Xunit;
 
 namespace System
 {
     public static partial class PlatformDetection
     {
-        public static bool IsWindowsIoTCore => false;
+        //
+        // Do not use the " { get; } = <expression> " pattern here. Having all the initialization happen in the type initializer
+        // means that one exception anywhere means all tests using PlatformDetection fail. If you feel a value is worth latching,
+        // do it in a way that failures don't cascade.
+        //
+
+        public static bool IsNetFramework => false;
         public static bool IsWindows => false;
         public static bool IsWindows7 => false;
-        public static bool IsWindows8x => false;
         public static bool IsWindows10Version1607OrGreater => false;
-        public static bool IsWindows10Version1703OrGreater => false;
-        public static bool IsWindows10Version1709OrGreater => false;
-        public static bool IsNotOneCoreUAP =>  true;
-        public static bool IsInAppContainer => false;
-        public static int WindowsVersion => -1;
+        public static bool IsWindows10Version20348OrGreater => false;
+        public static bool IsNotWindowsNanoServer => true;
+        public static bool IsNotWindowsServerCore => true;
 
-        public static bool IsCentos6 => IsDistroAndVersion("centos", 6);
-        public static bool IsAlpine => IsDistroAndVersion("alpine");
+        public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
         public static bool IsOpenSUSE => IsDistroAndVersion("opensuse");
         public static bool IsUbuntu => IsDistroAndVersion("ubuntu");
+        public static bool IsUbuntu2004 => IsDistroAndVersion("ubuntu", 20, 4);
         public static bool IsDebian => IsDistroAndVersion("debian");
-        public static bool IsDebian8 => IsDistroAndVersion("debian", 8);
-        public static bool IsUbuntu1404 => IsDistroAndVersion("ubuntu", 14, 4);
-        public static bool IsUbuntu1604 => IsDistroAndVersion("ubuntu", 16, 4);
-        public static bool IsUbuntu1704 => IsDistroAndVersion("ubuntu", 17, 4);
-        public static bool IsUbuntu1710 => IsDistroAndVersion("ubuntu", 17, 10);
-        public static bool IsUbuntu1710OrHigher => IsDistroAndVersionOrHigher("ubuntu", 17, 10);
-        public static bool IsUbuntu1804 => IsDistroAndVersion("ubuntu", 18, 04);
-        public static bool IsUbuntu1810OrHigher => IsDistroAndVersionOrHigher("ubuntu", 18, 10);
+        public static bool IsAlpine => IsDistroAndVersion("alpine");
+        public static bool IsDebian10 => IsDistroAndVersion("debian", 10);
+        public static bool IsRaspbian10 => IsDistroAndVersion("raspbian", 10);
+        public static bool IsMariner => IsDistroAndVersion("mariner");
+        public static bool IsSLES => IsDistroAndVersion("sles");
         public static bool IsTizen => IsDistroAndVersion("tizen");
         public static bool IsFedora => IsDistroAndVersion("fedora");
-        public static bool IsWindowsNanoServer => false;
-        public static bool IsWindowsServerCore => false;
-        public static bool IsWindowsAndElevated => false;
+        public static bool IsLinuxBionic => IsBionic();
+
+        public static bool IsMonoLinuxArm64 => IsMonoRuntime && IsLinux && IsArm64Process;
+        public static bool IsNotMonoLinuxArm64 => !IsMonoLinuxArm64;
+
+        // OSX family
+        public static bool IsOSXLike => IsOSX || IsiOS || IstvOS || IsMacCatalyst;
+        public static bool IsOSX => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        public static bool IsNotOSX => !IsOSX;
+        public static bool IsMacOsMojaveOrHigher => IsOSX && Environment.OSVersion.Version >= new Version(10, 14);
+        public static bool IsMacOsCatalinaOrHigher => IsOSX && Environment.OSVersion.Version >= new Version(10, 15);
+        public static bool IsMacOsAppleSilicon => IsOSX && IsArm64Process;
+        public static bool IsNotMacOsAppleSilicon => !IsMacOsAppleSilicon;
+        public static bool IsAppSandbox => Environment.GetEnvironmentVariable("APP_SANDBOX_CONTAINER_ID") != null;
+        public static bool IsNotAppSandbox => !IsAppSandbox;
 
         // RedHat family covers RedHat and CentOS
         public static bool IsRedHatFamily => IsRedHatFamilyAndVersion();
         public static bool IsNotRedHatFamily => !IsRedHatFamily;
-        public static bool IsRedHatFamily6 => IsRedHatFamilyAndVersion(6);
-        public static bool IsNotRedHatFamily6 => !IsRedHatFamily6;
         public static bool IsRedHatFamily7 => IsRedHatFamilyAndVersion(7);
+        public static bool IsCentos7 => IsDistroAndVersion("centos", 7);
         public static bool IsNotFedoraOrRedHatFamily => !IsFedora && !IsRedHatFamily;
+        public static bool IsNotDebian10 => !IsDebian10;
 
-        public static Version OSXVersion { get; } = ToVersion(Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.OperatingSystemVersion);
+        private static int GetWindowsProductType() => 0;
+        internal static bool IsWindowsVersionOrLater(int major, int minor, int build = -1) => false;
+        private static string GetWindowsInstallationType() => string.Empty;
 
-        public static string GetDistroVersionString()
+        private static Version ToVersion(string versionString)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            // In some distros/versions we cannot discover the distro version; return something valid.
+            // Pick a high version number, since this seems to happen on newer distros.
+            if (string.IsNullOrEmpty(versionString))
             {
-                return "OSX Version=" + s_osxProductVersion.ToString();
+                versionString = new Version(Int32.MaxValue, Int32.MaxValue).ToString();
             }
 
-            DistroInfo v = GetDistroInfo();
+            try
+            {
+                if (versionString.IndexOf('.') != -1)
+                    return new Version(versionString);
 
-            return "Distro=" + v.Id + " VersionId=" + v.VersionId;
+                // minor version is required by Version
+                // let's default it to 0
+                return new Version(int.Parse(versionString), 0);
+            }
+            catch (Exception exc)
+            {
+                throw new FormatException($"Failed to parse version string: '{versionString}'", exc);
+            }
         }
 
-        private static readonly Version s_osxProductVersion = GetOSXProductVersion();
-
-        public static bool IsMacOsHighSierraOrHigher { get; } =
-            IsOSX && (s_osxProductVersion.Major > 10 || (s_osxProductVersion.Major == 10 && s_osxProductVersion.Minor >= 13));
-
-        private static readonly Version s_icuVersion = GetICUVersion();
-        public static Version ICUVersion => s_icuVersion;
-
-        private static Version GetICUVersion()
+        /// <summary>
+        /// Assume that Android environment variables but Linux OS mean Android libc
+        /// </summary>
+        private static bool IsBionic()
         {
-            int ver = GlobalizationNative_GetICUVersion();
-            return new Version( ver & 0xFF,
-                               (ver >> 8)  & 0xFF,
-                               (ver >> 16) & 0xFF,
-                                ver >> 24);
+            if (IsLinux)
+            {
+                if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("ANDROID_STORAGE")))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        static Version ToVersion(string versionString)
+        private static DistroInfo GetDistroInfo()
         {
-            if (versionString.IndexOf('.') != -1)
-                return new Version(versionString);
+            DistroInfo result = new DistroInfo();
 
-            // minor version is required by Version
-            // let's default it to 0
-            return new Version(int.Parse(versionString), 0);
+            if (IsFreeBSD)
+            {
+                result.Id = "FreeBSD";
+                // example:
+                // FreeBSD 11.0-RELEASE-p1 FreeBSD 11.0-RELEASE-p1 #0 r306420: Thu Sep 29 01:43:23 UTC 2016     root@releng2.nyi.freebsd.org:/usr/obj/usr/src/sys/GENERIC
+                // What we want is major release as minor releases should be compatible.
+                result.VersionId = ToVersion(RuntimeInformation.OSDescription.Split()[1].Split('.')[0]);
+            }
+            else if (Isillumos)
+            {
+                // examples:
+                //   on OmniOS
+                //       SunOS 5.11 omnios-r151018-95eaa7e
+                //   on OpenIndiana Hipster:
+                //       SunOS 5.11 illumos-63878f749f
+                //   on SmartOS:
+                //       SunOS 5.11 joyent_20200408T231825Z
+                string versionDescription = RuntimeInformation.OSDescription.Split(' ')[2];
+                switch (versionDescription)
+                {
+                    case string version when version.StartsWith("omnios"):
+                        result.Id = "OmniOS";
+                        result.VersionId = ToVersion(version.Substring("omnios-r".Length, 2)); // e.g. 15
+                        break;
+                    case string version when version.StartsWith("joyent"):
+                        result.Id = "SmartOS";
+                        result.VersionId = ToVersion(version.Substring("joyent_".Length, 4)); // e.g. 2020
+                        break;
+                    case string version when version.StartsWith("illumos"):
+                        result.Id = "OpenIndiana";
+                        // version-less
+                        break;
+                }
+            }
+            else if (IsSolaris)
+            {
+                // example:
+                //   SunOS 5.11 11.3
+                result.Id = "Solaris";
+                // we only need the major version; 11
+                result.VersionId = ToVersion(RuntimeInformation.OSDescription.Split(' ')[2].Split('.')[0]); // e.g. 11
+            }
+            else if (File.Exists("/etc/os-release"))
+            {
+                foreach (string line in File.ReadAllLines("/etc/os-release"))
+                {
+                    if (line.StartsWith("ID=", StringComparison.Ordinal))
+                    {
+                        result.Id = line.Substring(3).Trim('"', '\'');
+                    }
+                    else if (line.StartsWith("VERSION_ID=", StringComparison.Ordinal))
+                    {
+                        result.VersionId = ToVersion(line.Substring(11).Trim('"', '\''));
+                    }
+                }
+            }
+
+            result.Id ??= "Linux";
+            result.VersionId ??= ToVersion(string.Empty);
+
+            return result;
         }
-
-        private static DistroInfo GetDistroInfo() => new DistroInfo()
-        {
-            Id = Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.OperatingSystem,
-            VersionId = ToVersion(Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.OperatingSystemVersion)
-        };
 
         private static bool IsRedHatFamilyAndVersion(int major = -1, int minor = -1, int build = -1, int revision = -1)
         {
@@ -106,7 +177,10 @@ namespace System
         /// Get whether the OS platform matches the given Linux distro and optional version.
         /// </summary>
         /// <param name="distroId">The distribution id.</param>
-        /// <param name="versionId">The distro version.  If omitted, compares the distro only.</param>
+        /// <param name="major">The distro major version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="minor">The distro minor version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="build">The distro build version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="revision">The distro revision version. If omitted, this portion of the version is not included in the comparison.</param>
         /// <returns>Whether the OS platform matches the given Linux distro and optional version.</returns>
         private static bool IsDistroAndVersion(string distroId, int major = -1, int minor = -1, int build = -1, int revision = -1)
         {
@@ -117,7 +191,10 @@ namespace System
         /// Get whether the OS platform matches the given Linux distro and optional version is same or higher.
         /// </summary>
         /// <param name="distroId">The distribution id.</param>
-        /// <param name="versionId">The distro version.  If omitted, compares the distro only.</param>
+        /// <param name="major">The distro major version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="minor">The distro minor version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="build">The distro build version. If omitted, this portion of the version is not included in the comparison.</param>
+        /// <param name="revision">The distro revision version.  If omitted, this portion of the version is not included in the comparison.</param>
         /// <returns>Whether the OS platform matches the given Linux distro and optional version is same or higher.</returns>
         private static bool IsDistroAndVersionOrHigher(string distroId, int major = -1, int minor = -1, int build = -1, int revision = -1)
         {
@@ -126,7 +203,7 @@ namespace System
 
         private static bool IsDistroAndVersion(Predicate<string> distroPredicate, int major = -1, int minor = -1, int build = -1, int revision = -1)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            if (IsLinux)
             {
                 DistroInfo v = GetDistroInfo();
                 if (distroPredicate(v.Id) && VersionEquivalentTo(major, minor, build, revision, v.VersionId))
@@ -140,7 +217,7 @@ namespace System
 
         private static bool IsDistroAndVersionOrHigher(Predicate<string> distroPredicate, int major = -1, int minor = -1, int build = -1, int revision = -1)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            if (IsLinux)
             {
                 DistroInfo v = GetDistroInfo();
                 if (distroPredicate(v.Id) && VersionEquivalentToOrHigher(major, minor, build, revision, v.VersionId))
@@ -170,67 +247,6 @@ namespace System
                                 (actualVersionId.Build == build && (actualVersionId.Revision > revision ||
                                     (actualVersionId.Revision == revision))))))));
         }
-
-        private static Version GetOSXProductVersion()
-        {
-            try
-            {
-                if (IsOSX)
-                {
-                    // <plist version="1.0">
-                    // <dict>
-                    //         <key>ProductBuildVersion</key>
-                    //         <string>17A330h</string>
-                    //         <key>ProductCopyright</key>
-                    //         <string>1983-2017 Apple Inc.</string>
-                    //         <key>ProductName</key>
-                    //         <string>Mac OS X</string>
-                    //         <key>ProductUserVisibleVersion</key>
-                    //         <string>10.13</string>
-                    //         <key>ProductVersion</key>
-                    //         <string>10.13</string>
-                    // </dict>
-                    // </plist>
-
-                    XElement dict = XDocument.Load("/System/Library/CoreServices/SystemVersion.plist").Root.Element("dict");
-                    if (dict != null)
-                    {
-                        foreach (XElement key in dict.Elements("key"))
-                        {
-                            if ("ProductVersion".Equals(key.Value))
-                            {
-                                XElement stringElement = key.NextNode as XElement;
-                                if (stringElement != null && stringElement.Name.LocalName.Equals("string"))
-                                {
-                                    string versionString = stringElement.Value;
-                                    if (versionString != null)
-                                    {
-                                        return Version.Parse(versionString);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch
-            {
-            }
-
-            // In case of exception or couldn't get the version 
-            return new Version(0, 0, 0);
-        }
-
-        [DllImport("libc", SetLastError = true)]
-        private static extern int sysctlbyname(string ctlName, byte[] oldp, ref IntPtr oldpLen, byte[] newp, IntPtr newpLen);
-
-        [DllImport("libc", SetLastError = true)]
-        internal static extern unsafe uint geteuid();
-
-        [DllImport("System.Globalization.Native", SetLastError = true)]
-        private static extern int GlobalizationNative_GetICUVersion();
-
-        public static bool IsSuperUser => geteuid() == 0;
 
         private struct DistroInfo
         {
