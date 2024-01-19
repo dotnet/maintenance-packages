d7cf6ee15e169a1e128aad23b5ae713381c5f401
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2024-01-19 15:41:21 -0800

    Migrate System.Xml.XPath.XmlDocument from 1.1

--- src/Common/tests/TestUtilities/System/AssertExtensions.cs
+++ src/Common/tests/TestUtilities/System/AssertExtensions.cs
@@ -1,12 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Xunit;
-using Xunit.Sdk;
 
 namespace System
 {
