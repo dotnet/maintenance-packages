6b6aa72553751fd61a5899f0bd2bd25c85f3580a
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2024-03-20 14:16:20 -0700

    Some temp adjustments in System.Data.SqlClient

---
+++ src/System.Data.SqlClient/src/System/Data/SqlClient/TemporaryUnixType.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Data.SqlClient
+{
+    public enum TemporaryUnixType
+    {
+        Unix0 = 0,
+        Unix1 = 1
+    }
+}
--- src/System.Data.SqlClient/tests/TempUnixTests.cs
+++ src/System.Data.SqlClient/tests/TempUnixTests.cs
@@ -1,18 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Runtime.Versioning;
 using Xunit;
 
 namespace System.Data.SqlClient.Tests
 {
-    public class TempTests
+    public class TempUnixTests
     {
         [Fact]
-        public void TempTest()
+        public void TempUnixTest()
         {
-            Assert.Equal(0, (int)TemporaryType.Val0);
-            Assert.Equal(1, (int)TemporaryType.Val1);
+            Assert.Equal(0, (int)TemporaryUnixType.Unix0);
+            Assert.Equal(1, (int)TemporaryUnixType.Unix1);
         }
     }
 }
